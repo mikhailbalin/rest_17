066ca3b65d9a5baaa17ff98254fe7c66dc6684c7
Misha Balin <m.balin@icloud.com>
2022-02-04 11:56:50 +0300

    customize cms Webpack config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,23 @@
 module.exports = {
-    siteMetadata: {
-      title: `Rest17`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
+  siteMetadata: {
+    title: `Rest17`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        customizeWebpackConfig: (config) => {
+          const { plugins } = config;
 
-    ]
-}
+          // MiniCssExtractPlugin breaks build
+          config.plugins = plugins.filter(
+            (plugin) =>
+              plugin.constructor &&
+              plugin.constructor.name !== "MiniCssExtractPlugin"
+          );
+        },
+      },
+    },
+  ],
+};

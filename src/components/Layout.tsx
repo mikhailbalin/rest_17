d52677e1a6f8ae7ba21d0950979c0d8294cb8709
Misha Balin <m.balin@icloud.com>
2022-02-07 13:26:24 +0300

    add useSiteMetadata

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,38 @@
 import React, { ReactNode } from "react";
 import { LightTheme, BaseProvider, LocaleProvider } from "baseui";
 import { localeOverrideRu } from "../locale/localeOverrideRu";
+import { Helmet } from "react-helmet";
+import { useSiteMetadata } from "../hooks/useSiteMetadata";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout = ({ children }: LayoutProps) => {
+  const { title, description } = useSiteMetadata();
+
   return (
     <BaseProvider theme={LightTheme}>
-      <LocaleProvider locale={localeOverrideRu}>{children}</LocaleProvider>
+      <LocaleProvider locale={localeOverrideRu}>
+        <Helmet>
+          <html lang="ru" />
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta charSet="utf-8" />
+
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+
+          <meta name="theme-color" content="#fff" />
+
+          <meta property="og:type" content="business.business" />
+          <meta property="og:title" content={title} />
+          <meta property="og:url" content="/" />
+        </Helmet>
+        {children}
+      </LocaleProvider>
     </BaseProvider>
   );
 };
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,13 @@
-import { createTheme } from "baseui";
+import {
+  createTheme,
+  createThemedStyled,
+  createThemedWithStyle,
+  createThemedUseStyletron,
+} from "baseui";
 import { Theme, ThemePrimitives } from "baseui/theme";
 import { DeepPartial } from "utility-types";
+
+type CustomTheme = Theme & { customColor: string };
 
 const primitives: Partial<ThemePrimitives> = {
   primaryFontFamily: "Jost",
@@ -14,4 +21,26 @@ const overrides: DeepPartial<Theme> = {
   },
 };
 
-export const theme = createTheme(primitives, overrides);
+const theme = createTheme(primitives, overrides);
+
+export const customTheme: CustomTheme = {
+  ...theme,
+  customColor: "pink",
+  breakpoints: {
+    ...theme.breakpoints,
+  },
+  mediaQuery: {
+    ...theme.mediaQuery,
+  },
+};
+
+export const themedStyled = createThemedStyled<CustomTheme>();
+export const themedWithStyle = createThemedWithStyle<CustomTheme>();
+export const themedUseStyletron = createThemedUseStyletron<CustomTheme>();
+
+/**
+ * 320
+ * 480
+ * 768
+ * 992
+ */

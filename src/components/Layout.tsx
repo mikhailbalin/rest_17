e2430004686618636da8c2023210d1242168d21b
Misha Balin <m.balin@icloud.com>
2022-02-07 14:36:16 +0300

    setup codegen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,12 +18,6 @@ export const Layout = ({ children }: LayoutProps) => {
           <html lang="ru" />
           <title>{title}</title>
           <meta name="description" content={description} />
-          <meta charSet="utf-8" />
-
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
 
           <meta name="theme-color" content="#fff" />
 
---
+++ src/hooks/__generated__/SITE_METADATA_QUERY.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: SITE_METADATA_QUERY
+// ====================================================
+
+export interface SITE_METADATA_QUERY_site_siteMetadata {
+  title: string | null;
+  description: string | null;
+}
+
+export interface SITE_METADATA_QUERY_site {
+  siteMetadata: SITE_METADATA_QUERY_site_siteMetadata | null;
+}
+
+export interface SITE_METADATA_QUERY {
+  site: SITE_METADATA_QUERY_site | null;
+}

a6526362dbb0e6ad98257117b4fb549ae66eb6f9
Misha Balin <m.balin@icloud.com>
2022-02-07 11:03:19 +0300

    add locale

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,15 @@
 import React, { ReactNode } from "react";
-import { LightTheme, BaseProvider } from "baseui";
+import { LightTheme, BaseProvider, LocaleProvider } from "baseui";
+import { localeOverrideRu } from "../locale/localeOverrideRu";
 
 type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout = ({ children }: LayoutProps) => {
-  return <BaseProvider theme={LightTheme}>{children}</BaseProvider>;
+  return (
+    <BaseProvider theme={LightTheme}>
+      <LocaleProvider locale={localeOverrideRu}>{children}</LocaleProvider>
+    </BaseProvider>
+  );
 };
